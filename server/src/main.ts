defa831c3133590473caa6e20e1048d7ccb73853
sunny-922 <sunny@sparcs.org>
2026-05-28 14:57:06 +0900

    post

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+  origin: 'http://localhost:5173', // 정확히 이 주소인지 확인
+  credentials: true,               // 쿠키나 세션을 사용한다면 필수
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
